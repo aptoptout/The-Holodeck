84a2330f206b93983cbf4007daba12165c8f73de
martijndeheer <martijndeheer@gmail.com>
2017-02-01 19:06:41 +0100

    Adding a working boolean

--- js/Experiment_01/Exp1.js
+++ js/Experiment_01/Exp1.js
@@ -120,8 +120,8 @@ var exp1 = function(p) {
     this.calculateAttraction = function(_m) {
       var force = p5.Vector.sub(this.position, _m.position);
       var distance = force.mag();
-      distance = p.constrain(distance, 2, 25);
-      var strength = (1 * 200 * 1) / (distance * distance);
+      distance = p.constrain(distance, 5, 25);
+      var strength = (1 * 2 * 0.1) / (distance * distance);
       force.mult(strength);
       return force;
     };
