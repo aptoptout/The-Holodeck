75837f7df8f76a7a778eca0cd2e6728fd233e8e8
martijndeheer <martijndeheer@gmail.com>
2017-02-23 22:24:14 +0100

    trying to make it work

--- js/sketch.js
+++ js/sketch.js
@@ -5,7 +5,6 @@
 // February 2017
 
 // This file links all the different 'sketches' (Exp1.js etc.) to load multiple in the same or different DIVs.
-// Wait for the page to load first
 var _element = document.getElementById("c1");
 var elementWidth = _element.offsetWidth;
 var elementHeight = _element.offsetHeight;
@@ -23,15 +22,9 @@ function showThisExperiment(event) {
             showThis = new p5(exp2, 'c1');
             break;
         default:
-            return false;
+            return true;
     }
 }
 
 var parent = document.getElementById('expIndex');
 parent.addEventListener('click', showThisExperiment);
-
-// if(_Exp1 == true) {
-//   experiment_1 = new p5(exp1, 'c1');
-// } else if(_Exp2 == true) {
-//   experiment_2 = new p5(exp2, 'c1');
-// }
