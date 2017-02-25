c3f1e53a94a5002cc2d0693b79d281f28573caa4
martijndeheer <martijndeheer@gmail.com>
2017-02-25 14:38:27 +0100

    EXPERIMENT 02 -- WEBGL issues.

--- js/Experiment_01/Exp1.js
+++ js/Experiment_01/Exp1.js
@@ -11,6 +11,22 @@
 // This sketch uses the instance mode for P5 to create multiple canvases in different DIVS on the page
 
 var exp1 = function(p) {
+  this.specs = {
+    id : "Experiment 01",
+    name : "Fast Connecting Dots",
+    description: "As the dots fly, \
+                  when close enough \
+                  they will connect \
+                  with each other, \
+                  creating a organic \
+                  chain of random events.\
+                  Hold your mouse inside \
+                  the canvas to attract \
+                  all the dots as the \
+                  mouse has gravitational attraction."
+    repository: "https://github.com/martijndeheer/The-Holodeck/blob/gh-pages/js/Experiment_01/Exp1.js"
+  };
+
   var bubbles = new Array(80);
   var attractor;
 
