ebf867899f6750e7df02fd03fc3f9b5999a74d6a
martijndeheer <martijndeheer@gmail.com>
2017-02-25 14:18:41 +0100

    EXPERIMENT 02 -- WEBGL issues.

--- js/Experiment_02/Exp2.js
+++ js/Experiment_02/Exp2.js
@@ -29,12 +29,13 @@ var exp2 = function(p) {
       p.rotateX(mapRotationY);
     }
 
-    p.noFill();
     p.stroke(255);
     p.strokeWeight(1);
-    // p.ambientLight(255, 255, 255);
+    p.ambientLight(255, 255, 255);
+    p.normalMaterial();
 
     p.beginShape();
+      p.fill(0, 64);
       p.vertex(0, 0, 0);
       p.vertex(100, 0, 0);
       p.vertex(100, 100, 0);
