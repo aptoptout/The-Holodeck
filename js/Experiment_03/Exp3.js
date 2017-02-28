80273e54432f927a93ae3941e9068cf360250648
martijndeheer <martijndeheer@gmail.com>
2017-02-28 22:45:27 +0100

    EXPERIMENT 03 -- Introducing points drawn on vertices of cube.

--- js/Experiment_03/Exp3.js
+++ js/Experiment_03/Exp3.js
@@ -51,7 +51,7 @@ var exp3 = function(target) {
 
     var _canvas = document.getElementsByTagName("canvas")[0].setAttribute("id", "defaultCanvas0");
 
-    group = new THREE.group();
+    group = new Three.Group();
 
     // Setting up the geometry to work with
     var geometry = new THREE.BoxGeometry(1, 1, 1, 10, 10, 10);
